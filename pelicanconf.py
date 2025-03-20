90b60a03b6f4474c7f81730112395736781ee19f
Katrina Ellison Geltman <katrina.m.ellison@gmail.com>
2025-03-20 12:47:55 -0400

    Add MONTH_ARCHIVE_SAVE_AS = "True" to pelicanconf.py

--- pelicanconf.py
+++ pelicanconf.py
@@ -2,3 +2,4 @@ THEME = '../pelican-dev-env/lib/python3.12/site-packages/pelican/themes/foundati
 SITENAME = "Katrina Ellison Geltman"
 FOUNDATION_FOOTER_TEXT = 'Powered by <a href="http://getpelican.com">Pelican</a> and <a href="http://foundation.zurb.com/">Zurb Foundation</a>. Theme by <a href="http://hamaluik.com">Kenton Hamaluik</a>.'
 FOUNDATION_PYGMENT_THEME = 'monokai'
+MONTH_ARCHIVE_SAVE_AS = 'True'
