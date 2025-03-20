88b52c620babd0d2c972206617f1812ec58729be
Katrina Ellison Geltman <katrina.m.ellison@gmail.com>
2025-03-20 18:38:25 -0400

    Add headshot

--- pelicanconf.py
+++ pelicanconf.py
@@ -3,3 +3,4 @@ SITENAME = "Katrina Ellison Geltman"
 FOUNDATION_FOOTER_TEXT = 'Powered by <a href="http://getpelican.com">Pelican</a> and <a href="http://foundation.zurb.com/">Zurb Foundation</a>. Theme by <a href="http://hamaluik.com">Kenton Hamaluik</a>.'
 FOUNDATION_PYGMENT_THEME = 'monokai'
 MONTH_ARCHIVE_SAVE_AS = 'True'
+STATIC_PATHS = ['theme/img']
